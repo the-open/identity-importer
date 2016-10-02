7fad8de0140c77756b699395239661ec4ba96ff2
Hiemanshu Sharma <hiemanshu@gmail.com>
2016-10-02 07:20:52 +0530

    Wrap insert tasks in transactions

--- lib/identity/importer/tasks/mailing_members.rb
+++ lib/identity/importer/tasks/mailing_members.rb
@@ -9,20 +9,23 @@ module Identity
           unsynced_mailings.each do |mailing|
             mailing_members = Identity::Importer.connection.run_query(sql(mailing.external_id))
 
-            mailing_members.each do |mailing_member|
-              member = Member.find_by(email: mailing_member['email'])
-              member_id = member.try(:id) || 1
+            ActiveRecord::Base.transaction do
+              mailing_members.each do |mailing_member|
+                member = Member.find_by(email: mailing_member['email'])
+                member_id = member.try(:id) || 1
 
-              member_mailing = MemberMailing.new
-              member_mailing.attributes = {
-                'mailing_id' => mailing.id,
-                'member_id' => member_id,
-                'external_id' => mailing_member['id']
-              }
-              member_mailing.save!
+                member_mailing = MemberMailing.new
+                member_mailing.attributes = {
+                  'mailing_id' => mailing.id,
+                  'member_id' => member_id,
+                  'external_id' => mailing_member['id']
+                }
+                member_mailing.save!
+              end
+              mailing.recipients_synced = true
+              mailing.save!
             end
-            mailing.recipients_synced = true
-            mailing.save!
+
           end
 
         end
--- lib/identity/importer/tasks/members.rb
+++ lib/identity/importer/tasks/members.rb
@@ -6,21 +6,24 @@ module Identity
         def self.run
           members = Identity::Importer.connection.run_query(sql)
 
-          members.each do |member_data|
-            data = {
-              name: (member_data['firstname'] or '(none)')+' '+(member_data['lastname'] or '(none)'),
-              email: member_data['email'],
-              created_at: member_data['created_at'],
-              updated_at: member_data['updated_at']
-            }
+          ActiveRecord::Base.transaction do
+            members.each do |member_data|
+              data = {
+                name: (member_data['firstname'] or '(none)')+' '+(member_data['lastname'] or '(none)'),
+                email: member_data['email'],
+                created_at: member_data['created_at'],
+                updated_at: member_data['updated_at']
+              }
 
-            member = Member.find_or_initialize_by(email: member_data['email'])
+              member = Member.find_or_initialize_by(email: member_data['email'])
 
-            if member.new_record?
-              member.attributes = data
-              member.save!
+              if member.new_record?
+                member.attributes = data
+                member.save!
+              end
             end
           end
+
         end
 
       end
