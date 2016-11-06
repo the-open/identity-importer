b4a699db3fef58304cbec687928c627fa43d7035
Hiemanshu Sharma <hiemanshu@gmail.com>
2016-11-07 04:25:23 +0530

    Use external_id to assign campaign to mailings

--- lib/identity/importer/tasks/mailings.rb
+++ lib/identity/importer/tasks/mailings.rb
@@ -11,7 +11,6 @@ module Identity
           'subject',      # the subject line (or lines)
           'body_html',    #
           'body_plain',   #
-          'campaign_id',
           'from',         # the fromline
           'created_at',   # when the mailing object was created
           'sent_at',      # when you actually hit send on the mailing
@@ -20,16 +19,17 @@ module Identity
 
         def self.run
           mailings = []
-          Identity::Importer.connection.run_query(sql).each do |row|
-            puts "Importing mailing with ID #{row['external_id']}"
-
-            mailing = Mailing.find_or_initialize_by(external_id: row['external_id'])
+          Identity::Importer.connection.run_query(sql).each do |mailing_data|
+            mailing = Mailing.find_or_initialize_by(external_id: mailing_data['external_id'])
             if mailing.new_record?
-              mailing.attributes = row.select do |column_name, value|
+              mailing.attributes = mailing_data.select do |column_name, value|
                 COLUMNS_TO_SELECT.include? column_name
               end
             end
 
+            campaign = Campaign.find_by(controlshift_campaign_id: action_data['campaign_id'])
+
+            mailing.campaign_id = campaign.try(:id)
             mailing.recipients_synced = false
             mailings << mailing
           end
