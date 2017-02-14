ef0005dbaea5af6dc7517e9cc7d6c3704b735043
Marcin Koziej <marcin@cahoots.pl>
2017-02-14 19:32:02 +0100

    Add counter

--- lib/identity/importer/tasks/mailing_members.rb
+++ lib/identity/importer/tasks/mailing_members.rb
@@ -13,7 +13,10 @@ module Identity
             mailing_members = Identity::Importer.connection.run_query(sql(mailing.external_id))
 
             ActiveRecord::Base.transaction do
+              counter = 0
               mailing_members.each_slice(10000) do |batch| 
+                logger.info "Iporting MemberMailing: Mailing #{mailing.id}. #{mailing.name}, done #{counter}"
+                counter += batch.length
                 batch.each do |mailing_member|
                   member_mailings = []
                   member = Member.find_by(email: mailing_member['email'])
