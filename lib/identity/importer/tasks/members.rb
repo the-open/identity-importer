104ac587e7a763860a37beb04592f2313288f28a
Marcin Koziej <marcin@cahoots.pl>
2017-04-11 16:06:30 +0530

    member import

--- lib/identity/importer/tasks/members.rb
+++ lib/identity/importer/tasks/members.rb
@@ -9,68 +9,59 @@ module Identity
           logger = Identity::Importer.logger
 
           got_members = Utils::member_cache
+          already_added_emails = Set.new
           members = Identity::Importer.connection.run_query(sql)
 
           email_subscription = Subscription.find(Subscription::EMAIL_SUBSCRIPTION)
+          if Identity::Importer.configuration.add_email_subscription and email_subscription.nil?
+            email_subscription = Subscription.create! id: Subscription::EMAIL_SUBSCRIPTION
+          end
 
           members.each_slice(1000) do |member_batch|
             ActiveRecord::Base.transaction do
               new_members = []
               new_member_subscriptions = []
               member_batch.each do |member_data|
-                data = {
-                  name: (member_data['firstname'] or '(none)')+' '+(member_data['lastname'] or '(none)'),
-                  email: member_data['email'],
-                  created_at: member_data['created_at'].try(:to_datetime),
-                  updated_at: member_data['updated_at'].try(:to_datetime)
-                }
+                next if already_added_emails.include? member_data['email']
 
                 member_in_id = got_members[member_data['email']]
                 if member_in_id
-                  # this contact is opt out.
-                  # we need to remove the subscription from Identity Id
-                  if member_data['is_opt_in'] == 1
-                    and not member_in_id[:email_subscription_id].nil?
 
-                    MemberSubscription.find(member_in_id[:email_subscription_id]).delete
-                    member_in_id[:email_subscription_id] = nil
-                  end
+                  if Identity::Importer.configuration.add_email_subscription
+                    # this contact is opt out.
+                    # we need to remove the subscription from Identity Id
+                    unless member_data['email_subscription']
+                      and not member_in_id[:email_subscription_id].nil?
 
-                  if member_data['is_opt_in'] == 0
-                    and member_in_id[:email_subscription_id].nil?
-                    # we have this member but does not have email subscription.
-                    # schedule to add it.
-                    new_member_subscriptions << MemberSubscription.new subscription: email_subscription, member_id: member_in_id[:id]
+                      MemberSubscription.find(member_in_id[:email_subscription_id]).delete
+                      member_in_id[:email_subscription_id] = nil
+                    end
+
+                    if member_data['email_subscription']
+                      and member_in_id[:email_subscription_id].nil?
+                      # we have this member but does not have email subscription.
+                      # schedule to add it.
+                      new_member_subscriptions << MemberSubscription.new subscription: email_subscription, member_id: member_in_id[:id]
+                    end
                   end
 
                 else
                   member = Member.new
-                  member.attributes = data
+                  member.attributes = {
+                    name: (member_data['firstname'] or '(none)')+' '+(member_data['lastname'] or '(none)'),
+                    email: member_data['email'],
+                    created_at: member_data['created_at'].try(:to_datetime),
+                    updated_at: member_data['updated_at'].try(:to_datetime)
+                  }
 
-                  if member_data['is_opt_out'] == 0
-                    new_member_subscriptions << MemberSubscription.new subscription: email_subscription, member_id: member_in_id[:id]
-                  end
-                end
-
-
-
-                if member.new_record?
-                  new_members << member
-                  logger.debug "Importing Member with email #{member.email}"
-                elsif member.changed?
-                  member.save
-                  logger.debug "Updating Member with email #{member.email}"
-                end
-
-                if Identity::Importer.configuration.add_email_subscription
-                  unless email_subscription.nil?
-                    member_subscription = MemberSubscription.find_or_initialize_by(subscription: email_subscription, member: member)
-                    if member_subscription.new_record?
-                      new_member_subscriptions << member_subscription
+                  if Identity::Importer.configuration.add_email_subscription
+                    if member_data['email_subscription']
+                      new_member_subscriptions << MemberSubscription.new subscription: email_subscription, member: member
                     end
                   end
-                end
 
+                  already_added_emails << member_data['email']
+                end
               end
               Member.import new_members
               MemberSubscription.import new_member_subscriptions
