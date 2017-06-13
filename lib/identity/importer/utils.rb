11015cb318d1a5b7bc11a529b98aa014d03e32f3
Marcin Koziej <marcin@akcjademokracja.pl>
2017-06-13 16:17:40 +0200

    fix: try to use Struct instead of map for member_cache
    This will be smaller in size

--- lib/identity/importer/utils.rb
+++ lib/identity/importer/utils.rb
@@ -9,15 +9,12 @@ module Identity
       end
 
       def self.member_cache
+        MemberCache = Struct.new(:id, :email_subscription_id, :unsubscribed_at)
+
         Hash[Member.
               joins("LEFT JOIN member_subscriptions ms ON ms.member_id = members.id AND ms.subscription_id = #{Subscription::EMAIL_SUBSCRIPTION}").
               select("members.*, ms.id as email_subscription_id, ms.unsubscribed_at as unsubscribed_at").
-              map { |m| [m.email, {
-                           id: m.id,
-                           email_subscription_id: m.email_subscription_id,
-                           unsubscribed_at: unsubscribed_at
-                         }]
-             }]
+              map { |m| [m.email, MemberCache.new(m.id, m.email_subscription_id, m.unsubscribed_at)]}]
       end
 
       def self.member_mailing_cache(mailing_id)
